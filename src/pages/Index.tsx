bf132c32a501896a6025832b453766490957f995
zibtalel <zibi@talelgym.tn>
2025-02-25 13:42:06 +0100

    kk

--- src/pages/Index.tsx
+++
@@ -1,5 +0,0 @@
-import { useState } from "react";
-import { JobCard } from "@/components/JobCard";
-import { SearchBar } from "@/components/SearchBar";
-import { Dialog, DialogContent } from "@/components/ui/dialog";
-import { Building2, MapPin } from "lucide-react";
